c1709103647b88de95e2e6ff8661ce80fe9bc1bb
Raghad-lababidi <raghad2lababidi@gmail.com>
2024-08-06 01:23:34 +0300

    first commit

---
+++ resources/views/cv.blade.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <style>
+            .page-break {
+                page-break-after: always;
+            }
+        </style>
+        <title>My Resume</title>
+    </head>
+    <body>
+        <div class="container " >
+            <div class="main-content mt-4" style="border: 1px solid;padding:15px; margin:30px 0">
+                
+                <div class="row justify-content-center mt-3">
+                    <div class="col-11">
+                        <div class="row" >
+                            <table>
+                                <tr>
+                                    <td>
+                                        <div class="name">
+                                            <h5 style="font-weight:700" class="m-0 p-0">Raghad Al Lababidi</h5>
+                                            <span class="m-0 p-0">backend laravel developer</span>
+                                        </div>
+                                        <span class="d-block">Damascus</span>
+                                    </td>
+                                    <td><span style="margin:0 30px"></span></td>
+                                    <td style="float:center;">
+                                        <h6><b>Email : </b>raghad2lababidi@gmail.com</h6>
+                                        <h6><b>LinkedIn : </b>https://www.linkedin.com/in/raghad-al-lababidi-6bb33324b</h6>
+                                        <h6><b>Phone : </b>+963996949135</h6>
+                                    </td>
+                                    <td><span style="margin:0 30px"></span></td>
+                                    <td style="float:right;">
+                                        <div class="image">
+                                            <img src="{{URL::asset('/image/photo.jpg')}}" alt="personal photo" height="150" width="150">
+                                        </div>
+                                    </td>
+                                </tr>
+
+                            </table>
+
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="row justify-content-center">
+                    <div class="col-md-11">
+                        <h4>Summary</h4>
+                        <p>Experienced in Laravel back-end development, building RESTful APIs and relational database building. Undergoing the process of being 
+a software engineer. I enjoy the process of problem analysis and problem solving. I always strive to learn more about my field so I can 
+excel at it. I manage to work and cope in team working environments.</p>
+                    </div>
+                </div>
+                <hr>
+                <div class="row justify-content-center">
+                    <div class="col-md-11">
+                        <h4>Education</h4>
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Degree / Diploma</th>
+                                    <th scope="col">Institue</th>
+                                    <th scope="col">Year</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Bachelor's degree in information tevhnology engineering</td>
+                                    <td>Damascus Universite</td>
+                                    <td>2023</td>
+                                </tr>
+                                <tr>
+                                    <td>high school certificate</td>
+                                    <td>Bahjat Albitar School</td>
+                                    <td>2017</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <hr>
+                <div class="row justify-content-center">
+                    <div class="col-md-11">
+                        <h4>Personal Project</h4>
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Project Name</th>
+                                    <th scope="col">Descreption</th>
+                                    <th scope="col">Year</th>
+                                </tr>
+
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>volunteer team management system</td>
+                                    <td>backend with laravel api and database mysql</td>
+                                    <td>2023</td>
+                                </tr>
+                                <tr>
+                                    <td>school management system</td>
+                                    <td>backend with laravel api and database mysql</td>
+                                    <td>2022</td>
+                                </tr>
+                                <tr>
+                                    <td>clinics reservation system</td>
+                                    <td>backend with laravel api and database mysql</td>
+                                    <td>2021</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <hr>
+                <div class="row justify-content-center">
+                    <div class="col-md-11">
+                        <h4>Technical Skills</h4>
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Type</th>
+                                    <th scope="col">Name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>programming languages</td>
+                                    <td>C++,C,php,java</td>
+                                </tr>
+                                <tr>
+                                    <td>database</td>
+                                    <td>mysql</td>
+                                </tr>
+                                <tr>
+                                    <td>git</td>
+                                    <td>github,gitlab</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <hr>
+                
+            </div> <!-- main-content -->
+        </div> <!-- container -->
+        <!-- ptional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    </body>
+</html>
